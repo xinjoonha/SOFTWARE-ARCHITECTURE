dbca48b42e355095606ab1c7aef9f88d5167cd00
xinjoonha <40624131@live.napier.ac.uk>
2024-11-20 18:11:46 +0000

    Project created

---
+++ PROJECT/SET10101/SET10101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SET10101
+//
+//  Created by 신준하 on 11/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PROJECT/SET10101/SET10101/SET10101App.swift
@@ -0,0 +1,17 @@
+//
+//  SET10101App.swift
+//  SET10101
+//
+//  Created by 신준하 on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SET10101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
